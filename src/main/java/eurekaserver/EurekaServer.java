0e7f574bf414f9f20aaba2992069a17fd0e5dd26
JJ Mubang <jmubang@gmail.com>
2018-06-14 15:25:59 -0400

    have eureka communicating with cloud config

--- src/main/java/eurekaserver/EurekaServer.java
+++ src/main/java/eurekaserver/EurekaServer.java
@@ -1,28 +1,15 @@
 package eurekaserver;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableEurekaServer
-@RestController
-@RefreshScope
+@SpringBootApplication
 public class EurekaServer {
-		
-	@Value("${message: Hello default}")
-	public String message;
 	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(EurekaServer.class, args);
-	}
-	
-	@RequestMapping("/hi")
-	public String getHi() {
-		return this.message;
 	}
 }
